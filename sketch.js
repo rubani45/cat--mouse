e697c17d006798b8f2b49bdcc2b10b6b38ebbf4e
rubani45 <77843138+rubani45@users.noreply.github.com>
2021-02-12 20:30:57 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,15 +3,15 @@ var catImg, catImg2, catImg3, mouseImg, mouseImg2, mouseImg3, bgImg;
 
 function preload() {
     //load the images here
-    catImg = loadAnimation("cat2.png","cat3.png");
-    catImg2 = loadImage("cat1.png");
-    catImg3 = loadImage("cat4.png");
+    catImg = loadAnimation("images/cat2.png","images/cat3.png");
+    catImg2 = loadImage("images/cat1.png");
+    catImg3 = loadImage("images/cat4.png");
 
-    mouseImg = loadAnimation("mouse2.png","mouse3.png");
-    mouseImg2 = loadImage("mouse1.png");
-    mouseImg3 = loadImage("mouse4.png");
+    mouseImg = loadAnimation("images/mouse2.png","images/mouse3.png");
+    mouseImg2 = loadImage("images/mouse1.png");
+    mouseImg3 = loadImage("images/mouse4.png");
 
-    bgImg = loadImage("garden.png");
+    bgImg = loadImage("images/garden.png");
 }
 
 function setup(){
@@ -34,22 +34,23 @@ function setup(){
 function draw() {
     background(255);
     //Write condition here to evalute if tom and jerry collide
-    if(cat.x-mouse.x<mouse.width/2+cat.width/2 &&
-        mouse.x-cat.x<mouse.width/2+cat.width/2){
+    if(mouse.x-cat.x<mouse.width/2-cat.width/2){
         cat.addImage("catStop",catImg3);
         cat.changeImage("catStop");
         cat.velocityX = 0;
         mouse.addImage("mouseStop",mouseImg3);
         mouse.changeAnimation("mouseStop");
     }
-
+     
+    console.log(mouse.x - cat.x);
+    console.log(mouse.width/2-cat.width/2);
     drawSprites();
 }
 
 
 function keyPressed(){
   //For moving and changing animation write code here
-  if(keyDown("left_arrow")){
+  if(keyCode===LEFT_ARROW){
       cat.velocityX = -5;
       cat.addAnimation("catRunning",catImg);
       cat.changeAnimation("catRunning");
